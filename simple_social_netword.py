7be50704fd882d28354fd38896ac331800934739
Nell Yonkos <nyonkos@terpmail.umd.edu>
2025-04-06 19:09:20 -0400

    added simple_social_netword.py

---
+++ simple_social_netword.py
@@ -0,0 +1,8 @@
+""" a simple social netword for tracking connections between people """
+
+class Person:
+    """ a person in the social network
+    attributes:
+        name (str): the person's name
+        connections (set of Person): other people in the social network who know this person    
+    """
